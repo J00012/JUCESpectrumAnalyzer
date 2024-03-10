2f7db4a60d6ea32f3d3189d1993cb2f7626e9c74
Rian Bennett <91235982+Rbenne0216@users.noreply.github.com>
2024-03-09 19:09:13 -0800

    Update PluginProcessor.cpp
    Moved procBlockIsRunning=true before for loop that gets buffer samples

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -13,17 +13,17 @@
 //==============================================================================
 FFTSpectrumAnalyzerAudioProcessor::FFTSpectrumAnalyzerAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       ),
+    : AudioProcessor(BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+        .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+        .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+    ),
     forwardFFT(fftOrder), // Initialize forwardFFT with fftOrder
     window(fftSize, juce::dsp::WindowingFunction<float>::hann) // Initialize window
-    
+
 #endif
 {
 }
@@ -40,29 +40,29 @@ const juce::String FFTSpectrumAnalyzerAudioProcessor::getName() const
 
 bool FFTSpectrumAnalyzerAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool FFTSpectrumAnalyzerAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool FFTSpectrumAnalyzerAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 double FFTSpectrumAnalyzerAudioProcessor::getTailLengthSeconds() const
@@ -73,7 +73,7 @@ double FFTSpectrumAnalyzerAudioProcessor::getTailLengthSeconds() const
 int FFTSpectrumAnalyzerAudioProcessor::getNumPrograms()
 {
     return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    // so this should be at least 1, even if you're not really implementing programs.
 }
 
 int FFTSpectrumAnalyzerAudioProcessor::getCurrentProgram()
@@ -81,21 +81,21 @@ int FFTSpectrumAnalyzerAudioProcessor::getCurrentProgram()
     return 0;
 }
 
-void FFTSpectrumAnalyzerAudioProcessor::setCurrentProgram (int index)
+void FFTSpectrumAnalyzerAudioProcessor::setCurrentProgram(int index)
 {
 }
 
-const juce::String FFTSpectrumAnalyzerAudioProcessor::getProgramName (int index)
+const juce::String FFTSpectrumAnalyzerAudioProcessor::getProgramName(int index)
 {
     return {};
 }
 
-void FFTSpectrumAnalyzerAudioProcessor::changeProgramName (int index, const juce::String& newName)
+void FFTSpectrumAnalyzerAudioProcessor::changeProgramName(int index, const juce::String& newName)
 {
 }
 
 //==============================================================================
-void FFTSpectrumAnalyzerAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+void FFTSpectrumAnalyzerAudioProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
@@ -108,62 +108,77 @@ void FFTSpectrumAnalyzerAudioProcessor::releaseResources()
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
-bool FFTSpectrumAnalyzerAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool FFTSpectrumAnalyzerAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
+#if JucePlugin_IsMidiEffect
+    juce::ignoreUnused(layouts);
     return true;
-  #else
+#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
     // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+#if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
+#endif
 
     return true;
-  #endif
+#endif
 }
 #endif
+
+void FFTSpectrumAnalyzerAudioProcessor::resetProcBlockIsRunning()
+{
+    procBlockIsRunning = false;
+}
+
+bool FFTSpectrumAnalyzerAudioProcessor::getProcBlockIsRunning()
+{
+    return procBlockIsRunning;
+}
 
 
 //buffer- two dimenstional array where rows represent different channels and columns represent individual samples
 //(A multi-channel buffer containing floating point audio samples)
 //
 //midiMessages
-void FFTSpectrumAnalyzerAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void FFTSpectrumAnalyzerAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
 
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-   //loops through the output channels and clears a specified region of all the channels
-    //for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        //buffer.clear (i, 0, buffer.getNumSamples());
-
-
-    //TEST CODE !!!!!
 
     //just for mono
     int channel = 0;
+    std::cout << "hello test";
 
     auto* channelData = buffer.getReadPointer(channel);
 
+    //loops through the output channels and clears a specified region of all the channels
+     //for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+         //buffer.clear (i, 0, buffer.getNumSamples());
+
+
+     //TEST CODE !!!!!
     /*for (int sample = 0; sample < buffer.getNumSamples(); ++sample) {
         fftArray[sample] = channelData[sample];
     }*/
+
+    procBlockIsRunning = true;
+
     for (int sample = 0; sample < buffer.getNumSamples(); ++sample) {
         scopeData[sample] = channelData[sample];
     }
-    
+
+    //resetProcBlockIsRunning();
+
     //memcpy(fftData, fftArray, sizeof(fftArray));
 
 
@@ -219,18 +234,18 @@ bool FFTSpectrumAnalyzerAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* FFTSpectrumAnalyzerAudioProcessor::createEditor()
 {
-    return new FFTSpectrumAnalyzerAudioProcessorEditor (*this);
+    return new FFTSpectrumAnalyzerAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void FFTSpectrumAnalyzerAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+void FFTSpectrumAnalyzerAudioProcessor::getStateInformation(juce::MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
 }
 
-void FFTSpectrumAnalyzerAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void FFTSpectrumAnalyzerAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
