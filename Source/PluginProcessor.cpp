16cf65758864611eb50298ac81f19e48e946819e
Mykah Marks <marks038@csusm.edu>
2024-04-08 20:41:55 -0700

    plots single FFT plot

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -177,13 +177,6 @@ void FFTSpectrumAnalyzerAudioProcessor::zeroAllSelections(int binMagSize, int se
 
 void FFTSpectrumAnalyzerAudioProcessor::prepSelection(int binMagSize, int selectionSize, int selectionIndex) {
     binMag.resize(selectionSize, std::vector<float>(binMagSize));
-    zeroSelection(selectionIndex, binMagSize);
-    bufferLeft.resize(fftSize, 0.0f);
-    bufferRight.resize(fftSize, 0.0f);
-    windowBufferRight.resize(fftDataSize, 0.0f);
-    windowBufferLeft.resize(fftSize, 0.0f);
-
-    rowIndex = selectionIndex;
     if (rowIndex != 0) {
         zeroSelection(selectionIndex, binMagSize);
     }
